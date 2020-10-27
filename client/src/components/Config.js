c985878eb229e05ec0589d37b367ee1e3482828d
Kang Yerim <yerim25@gmail.com>
2020-10-27 11:40:07 +0900

    Landing Page added

---
+++ client/src/components/Config.js
@@ -0,0 +1,5 @@
+//SERVER ROUTES
+export const USER_SERVER = "/api/users";
+export const API_URL = "https://api.themoviedb.org/3/";
+export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
+export const API_KEY = "a72a7624e0ac422a29e55d3a8a459d8b";
---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import { FaCode } from "react-icons/fa";
+import { API_URL, API_KEY, IMAGE_BASE_URL } from "../../Config";
+import MainImage from "./Sections/MainImage";
+
+function LandingPage() {
+  const [Movies, setMovies] = useState([]);
+  const [MainMovieImage, setMainMovieImage] = useState(null);
+  useEffect(() => {
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    fetch(endpoint)
+      .then((response) => response.json())
+      .then((response) => {
+          console.log(response)
+        setMovies([response.results]);
+        setMainMovieImage(response.results[0]);
+      });
+  }, []);
+  return (
+    <div style={{ width: "100%", margin: "0" }}>
+      {/* Main Image */}
+      {MainMovieImage && ( // MainMovieImage가 있으면 저장
+        <MainImage
+          image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
+          title={MainMovieImage.original_title}
+          text={MainMovieImage.overview}
+        />
+      )}
+
+      <div style={{ width: "85%", margin: "1rem auto" }}>
+        <h2>Movies by latest</h2>
+        <hr></hr>
+        {/* Movie Grid Cards */}
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <button>Load More</button>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
